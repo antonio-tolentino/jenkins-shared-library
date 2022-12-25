6d22bb4021fdfa185b4948846cb60fde38930bda
Antonio Tolentino de Castro Junior <atcjunior@gmail.com>
2022-12-25 18:44:50 +0100

    Jenkins shared library

--- vars/incVersion.groovy
+++ vars/incVersion.groovy
@@ -1,6 +1,6 @@
 def String call( Map config=[:] ) {
 
-  def pattern =~ /^(\d{1,3})\.(\d{1,3})\.(\d{1,4})$/
+  def pattern = /^(\d{1,3})\.(\d{1,3})\.(\d{1,4})$/
 
   return  config.version.replaceFirst(pattern) { _,major,minor -> "${major}.${minor}.0"}
 
