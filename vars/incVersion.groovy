471dc40950b27594085ca910ee7b9936270d2fc7
Antonio Tolentino de Castro Junior <atcjunior@gmail.com>
2022-12-25 18:11:21 +0100

    Jenkins shared library

--- vars/incVersion.groovy
+++ vars/incVersion.groovy
@@ -4,14 +4,14 @@ def String call( Map config=[:] ) {
 
 
 
-  switch(map.incType.toUpperCase()) {
+  switch(config.incType.toUpperCase()) {
     case "MAJOR":
-      return map.version.replaceFirst(pattern) { _,major,minor,patch -> "${(major as int) + 1}.0.0"}
+      return config.version.replaceFirst(pattern) { _,major,minor,patch -> "${(major as int) + 1}.0.0"}
     case "MINOR":
-      return  map.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${(minor as int) + 1}.0"}
+      return  config.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${(minor as int) + 1}.0"}
     case "PATCH":
-      return map.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${minor}.${(patch as int) + 1}"}
+      return config.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${minor}.${(patch as int) + 1}"}
     default:
-      return map.version
+      return config.version
   }
 }
