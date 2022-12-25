ac4b5779e61ae4f8917f6cea3d1c6d477f27fe3a
Antonio Tolentino de Castro Junior <atcjunior@gmail.com>
2022-12-25 18:51:08 +0100

    Jenkins shared library

--- vars/incVersion.groovy
+++ vars/incVersion.groovy
@@ -1,8 +1,16 @@
 def String call( Map config=[:] ) {
 
-  def pattern = /^(\d{1,3})\.(\d{1,3})\.(\d{1,4})$/
+  def versionParts = config.version.tokenize('.')
 
-  return  config.version.replaceFirst(pattern) { _,major,minor -> "${major}.${minor}.0"}
+  println versionParts
+
+  if (versionParts.size != 3) {
+      throw new IllegalArgumentException("Wrong version format - expected MAJOR.MINOR.PATCH - got ${version}")
+  }
+
+  def major = versionParts[0].toInteger()
+  def minor = versionParts[1].toInteger()
+  def patch = versionParts[2].toInteger()
 
  
   //switch(config.incType.toUpperCase()) {
