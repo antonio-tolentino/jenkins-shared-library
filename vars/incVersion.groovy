2360222bc4621191520cf1fb9e285773bcbd5f6b
Antonio Tolentino de Castro Junior <atcjunior@gmail.com>
2022-12-25 18:25:58 +0100

    Jenkins shared library

--- vars/incVersion.groovy
+++ vars/incVersion.groovy
@@ -1,21 +1,20 @@
 def String call( Map config=[:] ) {
 
-  println ("debug = " + config.version)
   def pattern = ~/(\d{1,3})\.(\d{1,3})\.\d{1,4}$/
 
+  return  config.version.replaceFirst(pattern) { _,major,minor -> "${major}.${(minor as int) + 1}.0"}
 
+ 
+  //switch(config.incType.toUpperCase()) {
+  //  case "MAJOR":
+  //    return config.version.replaceFirst(pattern) { _,major,minor,patch -> "${(major as int) + 1}.0.0"}
+  //  case "MINOR":
+  //    return  config.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${(minor as int) + 1}.0"}
+  //  case "PATCH":
+  //    return config.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${minor}.${(patch as int) + 1}"}
+  //  default:
+  //    return config.version
+  //}
 
-  switch(config.incType.toUpperCase()) {
-    case "MAJOR":
-      return config.version.replaceFirst(pattern) { _,major,minor,patch -> "${(major as int) + 1}.0.0"}
-    case "MINOR":
-      return  config.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${(minor as int) + 1}.0"}
-    case "PATCH":
-      return config.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${minor}.${(patch as int) + 1}"}
-    default:
-      return config.version
-  }
-
-  def newVersion = version.replaceFirst(pattern) { _,major,minor -> "${major}.${(patch as int) + 1}.0"}
 }
 
