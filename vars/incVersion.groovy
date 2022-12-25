d56433fd526769a5860c0c2d17c1f7bf4487cce8
Antonio Tolentino de Castro Junior <atcjunior@gmail.com>
2022-12-25 18:54:41 +0100

    Jenkins shared library

--- vars/incVersion.groovy
+++ vars/incVersion.groovy
@@ -2,9 +2,7 @@ def String call( Map config=[:] ) {
 
   def versionParts = config.version.tokenize('.')
 
-  println versionParts
-
-  if (versionParts.size != 3) {
+   if (versionParts.size != 3) {
       throw new IllegalArgumentException("Wrong version format - expected MAJOR.MINOR.PATCH - got ${version}")
   }
 
@@ -13,16 +11,16 @@ def String call( Map config=[:] ) {
   def patch = versionParts[2].toInteger()
 
  
-  //switch(config.incType.toUpperCase()) {
-  //  case "MAJOR":
-  //    return config.version.replaceFirst(pattern) { _,major,minor,patch -> "${(major as int) + 1}.0.0"}
-  //  case "MINOR":
-  //    return  config.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${(minor as int) + 1}.0"}
-  //  case "PATCH":
-  //    return config.version.replaceFirst(pattern) { _,major,minor,patch -> "${major}.${minor}.${(patch as int) + 1}"}
-  //  default:
-  //    return config.version
-  //}
+  switch(config.incType.toUpperCase()) {
+    case "MAJOR":
+      return "${major + 1}.0.0"
+    case "MINOR":
+      return "${major}.${minor + 1}.0"
+    case "PATCH":
+      return "${major}.${minor}.${patch + 1}"
+    default:
+      return config.version
+  }
 
 }
 
